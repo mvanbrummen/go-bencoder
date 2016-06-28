d8ad19048eaab32de454bac94ea3b737ff1e6677
Michael Van Brummen <michaelvanbrummen@gmail.com>
2016-06-28 20:37:28 +1000

    Implemented encode function

--- encode.go
+++ encode.go
@@ -3,3 +3,44 @@
 // license that can be found in the LICENSE file.
 
 package bencode
+
+import (
+  "bytes"
+  "fmt"
+)
+
+func BeEncode(data interface{}) (bencoding []byte, err error) {
+  defer func() {
+    if ex := recover(); ex != nil {
+      err = fmt.Errorf("%v", ex)
+    }
+  }()
+  bencoding = encodeEntity(data)
+  return bencoding, nil
+}
+
+func encodeEntity(data interface{}) []byte {
+  var buf bytes.Buffer
+  switch v := data.(type) {
+  case string:
+    buf.WriteString(fmt.Sprintf("%d:%s", len(v), v))
+  case int, uint, int8, int16, int32, int64, uint8, uint16, uint32, uint64:
+    buf.WriteString(fmt.Sprintf("i%de", v))
+  case []interface{}:
+    buf.WriteRune('l')
+    for _, item := range v {
+      buf.Write(encodeEntity(item))
+    }
+    buf.WriteRune('e')
+  case map[string]interface{}:
+    buf.WriteRune('d')
+    for k, v := range v {
+      buf.Write(encodeEntity(k))
+      buf.Write(encodeEntity(v))
+    }
+    buf.WriteRune('e')
+  default:
+    panic(fmt.Sprintf("Failed to BEncode type: %T", data))
+  }
+  return buf.Bytes()
+}
--- encode_test.go
+++ encode_test.go
@@ -35,3 +35,24 @@ func TestEncode(t *testing.T) {
 		}
 	}
 }
+
+type encodeTestpair2 struct {
+	Value interface{}
+	Expected string
+}
+
+var encodeTests2 = []encodeTestpair2{
+	{"spam", "4:spam"},
+	{123, "i123e"},
+	{[]interface{}{"spam", 123}, "l4:spami123ee"},
+	{map[string]interface{}{"spam": 123}, "d4:spami123ee"},
+}
+
+func TestEncodeInterface(t *testing.T) {
+	for _, pair := range encodeTests2 {
+		b, _ := BeEncode(pair.Value)
+		if string(b) != pair.Expected {
+			t.Error("For", pair.Value, "Expected", pair.Expected, "Got", string(b))
+		}
+	}
+}
